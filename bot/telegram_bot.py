ab6dcfc3f826d7c8bed52867be0519dbff4dfc17
Z3nd <jonhrip9@gmail.com>
2023-04-23 14:07:32 +0300

    Added a warning message that you cannot convert to audio

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -70,6 +70,9 @@ class TelegramBot:
         logging.warning(
             f'Request to be converted into audio from {callback.from_user.username} (id: {callback.from_user.id})')
         voice = self.announcer.voicing(callback.message.text, callback.from_user.id)
+        if voice is None:
+            await self.bot.send_message(callback.from_user.id, "Unfortunately, I can't recognize this message")
+            return
         ogg_file = types.InputFile(voice)
 
         await self.bot.send_voice(callback.from_user.id, ogg_file)
