b68b144750bed3a562370e20bc6874056f5fa6e3
Z3nd <jonhrip9@gmail.com>
2023-04-26 21:33:27 +0300

    del history

--- bot/main.py
+++ bot/main.py
@@ -48,19 +48,17 @@ def main():
     # Setup configurations
     openai_config = {"token_openai": os.environ['TOKEN_OPENAI'],
                      'model': os.environ.get('MODEL', 'gpt-3.5-turbo-0301'),
-                     'history': os.environ.get('HISTORY_FILE', 'history/history.json'),
                      'image_size': os.environ.get('IMAGE_SIZE', '512x512'),
                      'max_tokens': int(os.environ.get('MAX_TOKENS', 1200)),
                      'max_all_tokens': int(os.environ.get('MAX_ALL_TOKENS', 4097)),
-                     'n_choices': int(os.environ.get('N_CHOICES', 1)),
                      'temperature': float(os.environ.get('TEMPERATURE', 1.0)),
                      'presence_penalty': float(os.environ.get('PRESENCE_PENALTY', 0.0)),
                      'frequency_penalty': float(os.environ.get('FREQUENCY_PENALTY', 0.0)),
-                     'stream': os.environ.get('STREAM', 'true'),
                      }
 
     telegram_config = {'token_bot': os.environ['TOKEN_TELEGRAM'],
-                       'allowed_user_ids': os.environ.get('ALLOWED_TELEGRAM_USER_IDS', '*')}
+                       'allowed_user_ids': os.environ.get('ALLOWED_TELEGRAM_USER_IDS', '*'),
+                       'stream': os.environ.get('STREAM', 'true').lower() == "true"}
 
     voicing_config = {'ru_model_speech': os.environ.get('RU_MODEL_SPEECH', 'models/ru_v3.pt'),
                       'en_model_speech': os.environ.get('EN_MODEL_SPEECH', 'models/v3_en.pt'),
